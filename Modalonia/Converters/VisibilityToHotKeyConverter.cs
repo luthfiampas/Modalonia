54a1a0ce11b0855bd09ae3670d2fc5b2845fe652
luthfiampas <luthfi.personal@gmail.com>
2021-03-17 21:46:31 +0700

    hotkey for Yes and Ok command

---
+++ Modalonia/Converters/VisibilityToHotKeyConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Avalonia.Input;
+
+namespace Modalonia.Converters
+{
+    internal class VisibilityToHotKeyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool) value ? KeyGesture.Parse((string) parameter) : null;
+        }
+        
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
